b59660c2dcbe3f4c23c5f3f173e561ed452908bd
harshitbansal373 <harshitbansal373@gmail.com>
2019-09-08 01:20:29 +0530

    tic-tac-toe

---
+++ tic-tac-toe.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import tkinter.messagebox
+root=Tk()
+root.title('tik-tak-toe')
+root.config(bg='#fffaaa')
+img1=PhotoImage(file='o.png')
+img2=PhotoImage(file='x.png')
+
+root.mainloop()
