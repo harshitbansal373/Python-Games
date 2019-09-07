fd5241c77905c6cd2159c4e38bac222beb50e9f4
harshitbansal373 <harshitbansal373@gmail.com>
2019-09-08 01:04:03 +0530

    buttons and label

--- Guess-table.py
+++ Guess-table.py
@@ -2,4 +2,14 @@ from tkinter import *
 root=Tk()
 root.title('Guess Table')
 root.config(bg='sky blue')
+for i in range(1,11):
+  b=Label(root,text=str(i),bg='#fffaaa',fg='black',font='arial 35 italic')
+  b.grid(row=0,column=i-1,padx=10,pady=10,sticky='nswe')
+for i in range(1,10):
+  b=Label(root,text=str(i+1),bg='#fffaaa',fg='black',font='arial 35 italic')
+  b.grid(row=i,column=0,padx=10,pady=10,sticky='nswe')
+for i in range(1,10):
+  for j in range(1,10):
+    b=Button(root,text='?',bg='#fffaaa',fg='black',font='arial 34 italic',command=lambda t=(i,j):display(t))
+    b.grid(row=i,column=j,padx=10,pady=10,sticky='nswe')
 root.mainloop()
