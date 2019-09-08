c3eec3bfe203ac0d674fdad71b0017086e0989e9
harshitbansal373 <harshitbansal373@gmail.com>
2019-09-08 21:23:13 +0530

    tic-tac-toe

--- Tic-Tac-Toe/tic-tac-toe.py
+++ Tic-Tac-Toe/tic-tac-toe.py
@@ -37,19 +37,27 @@ def reset():
   intial()
 
 def check():
+  global sc1
+  global sc2
+  global pl1
+  global pl2
   win=[{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{6,4,2}]
   if p==True:
     for s in win:
       if s<=p2:
-        tkinter.messagebox.showinfo('result','player2 won!!!')
+        tkinter.messagebox.showinfo('Result','Player2 won!!!')
+        sc2=sc2+1
+        pl2['text']='Player2 : '+str(sc2)
         reset()
   else:
     for s in win:
       if s<=p1:
-        tkinter.messagebox.showinfo('result','player1 won!!!')
+        tkinter.messagebox.showinfo('Result','Player1 won!!!')
+        sc1=sc1+1
+        pl1['text']='Player1 : '+str(sc1)
         reset()
   if count==9:
-    tkinter.messagebox.showinfo('result','draw')
+    tkinter.messagebox.showinfo('Result','Draw')
     reset()
 
 def solve(t):
@@ -77,9 +85,20 @@ count=0
 p=True
 p1=set()
 p2=set()
+sc1=0
+sc2=0
 intial()
-sb=Button(root,width=10,height=5,text='start',bg='#abcdef',command=start)
-sb.grid(row=3,column=1,padx=5,pady=5,sticky='nswe')
+
+f=Frame(root)
+pl1=Label(f,text='Player1 : 0',bg='#abcdef',height=5)
+pl1.pack(side='left',padx=5,pady=5,expand=YES,fill=BOTH)
+sb=Button(f,text='Start',bg='#abcdef',height=5,command=start)
+sb.pack(side='left',padx=5,pady=5,expand=YES,fill=BOTH)
+rb=Button(f,text='Restart',bg='#abcdef',height=5)
+rb.pack(side='left',padx=5,pady=5,expand=YES,fill=BOTH)
+pl2=Label(f,text='Player2 : 0',bg='#abcdef',height=5)
+pl2.pack(side='left',padx=5,pady=5,expand=YES,fill=BOTH)
+f.grid(row=3,column=0,columnspan=3,sticky='nswe')
 
 for i in range(3):
   root.grid_rowconfigure(i,weight=1)
